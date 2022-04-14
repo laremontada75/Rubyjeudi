3e7871e4b8f74c27b8552533abd2358c64ef6ca2
laremontada75 <103064845+laremontada75@users.noreply.github.com>
2022-04-14 23:39:20 +0200

    Add files via upload
    Soyez indulgents svpp

---
+++ 00_journalists.rb
@@ -0,0 +1,84 @@
+blases = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def taille(blases)
+    return blases.length
+end
+
+def lepluscourt(bl)
+    return blases.sort_by(&:length)[0]
+end
+
+def handle5caractere
+    return blases.select{ |n| n.size ==6 }
+    return blases.count{ |n| n.size ==6 }
+end
+def avec_une_maj
+    return blases.count {|e| e =~ /^@[A-Z]/}
+end
+def ordrealphabétique(bl)
+    return blases.sort
+end
+def ordreparlongueur
+    blases.sort_by(&:length)
+end
+def position_handle
+    return blases.index("@epenser")
+end
+
+def répartition_handle 
+    return blases.count{ |n| n.size ==1 }
+    return blases.count{ |n| n.size ==2 }
+    return blases.count{ |n| n.size ==3 }
+    return blases.count{ |n| n.size ==4 }
+    return blases.count{ |n| n.size ==5 }
+    return blases.count{ |n| n.size ==6 }
+    return blases.count{ |n| n.size ==7 }
+    return blases.count{ |n| n.size ==8 }
+    return blases.count{ |n| n.size ==9 }
+    return blases.count{ |n| n.size ==10 }
+    return blases.count{ |n| n.size ==11 }
+    return blases.count{ |n| n.size ==12 }
+    return blases.count{ |n| n.size ==13 }
+    return blases.count{ |n| n.size ==14 }
+    return blases.count{ |n| n.size ==15 } 
+    return blases.count{ |n| n.size ==16 }
+    return blases.count{ |n| n.size ==17 }
+    return blases.count{ |n| n.size > 17 }
+end
+
+def perform(blases)
+    puts "1.Nombre de handle dans le hash #{blases.length}"
+    puts "2.Le handle le plus court est #{blases.sort_by(&:length)[0]}, le plus long est #{blases.sort_by(&:length)[380]}"
+    puts "3.Voici la liste des éléments qui ont cinq éléments #{blases.select{ |n| n.size ==6 }}, 
+    il y en a #{blases.count{ |n| n.size ==6} }"
+    puts "4.Il y en a #{blases.count {|e| e =~ /^@[A-Z]/}} qui commencent par une majuscule"
+    puts "5. et voici la liste triée par ordre alphabétique ! 
+    #{blases.sort}"
+    puts " "
+    puts "6. et voici la liste triée du plus petit au plus grand nom 
+    #{blases.sort_by(&:length)}"
+    puts "7. Voici la position dans l'index du handle @epenser #{blases.index("@epenser")}"
+    puts "8. Et voici la répartition des éléments par taille de ces derniers
+    handle de 1 caractère #{blases.count{ |n| n.size ==1 }}
+    handle de 2 caractères #{blases.count{ |n| n.size ==2 }}
+    handle de 3 caractères #{blases.count{ |n| n.size ==3 }}
+    handle de 4 caractères #{blases.count{ |n| n.size ==4 }}
+    handle de 5 caractères #{blases.count{ |n| n.size ==5 }}
+    handle de 6 caractères #{blases.count{ |n| n.size ==6 }}
+    handle de 7 caractères #{blases.count{ |n| n.size ==7 }}
+    handle de 8 caractères #{blases.count{ |n| n.size ==8 }}
+    handle de 9 caractères #{blases.count{ |n| n.size ==9 }}
+    handle de 10 caractères #{blases.count{ |n| n.size ==10 }}
+    handle de 11 caractères #{blases.count{ |n| n.size ==11 }}
+    handle de 12 caractères #{blases.count{ |n| n.size ==12 }}
+    handle de 13 caractères #{blases.count{ |n| n.size ==13 }}
+    handle de 14 caractères #{blases.count{ |n| n.size ==14 }}
+    handle de 15 caractères #{blases.count{ |n| n.size ==15 }}
+    handle de 16 caractères #{blases.count{ |n| n.size ==16 }}
+    handle de 17 caractères #{blases.count{ |n| n.size ==17 }}
+    handle de plus de 17 caractères #{blases.count{ |n| n.size > 17 }}"
+end
+
+
+perform(blases)
+
